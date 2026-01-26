aaceec4a2bd98bb9b5e03aa78921c702aecd2862
Baibhav Acharyya <baibhav.acharyya@gmail.com>
2026-01-26 18:11:10 +0530

    03: database setup

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,14 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { auth, clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher([
+  "/api/innges(.*)",
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    await auth.protect();
+  }
+});
 
 export const config = {
   matcher: [
